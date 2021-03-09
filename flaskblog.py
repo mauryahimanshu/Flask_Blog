e889a936149cc7068921e04ddd4a752c51cfe37c
Himanshu Maurya <himanshu.mmmut@gmail.com>
2021-03-09 19:21:08 +0530

    Added Templates and Static files

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello World!</h1>'
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
