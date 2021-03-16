686d3c4486db74ed4e4b8c5a6cdbfc81b0bb5f42
Himanshu Maurya <himanshu.mmmut@gmail.com>
2021-03-17 00:26:10 +0530

    Facility for User to update their profile

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 from flaskblog.models import User, Post
+from flask_login import current_user
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=10)])
@@ -20,8 +23,28 @@ class RegistrationForm(FlaskForm):
         if(user):
             raise ValidationError('This email is already taken! Please choose a different one.')
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class ProfileUpdateForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=10)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    image = FileField('Update Profile Pic', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if(user):
+                raise ValidationError('This username is already taken! Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if(user):
+                raise ValidationError('This email is already taken! Please choose a different one.')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,8 @@
+import secrets
+import os
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, ProfileUpdateForm
 from flaskblog import app, bcrypt, db
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
@@ -50,7 +53,41 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/account')
+
+def save_image(form_image):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_image.filename)
+    image_file_name = random_hex+f_ext
+    image_path = os.path.join(app.root_path, 'static/profile_pics', image_file_name)
+
+    output_size = (500, 500)
+    resized_image = Image.open(form_image)
+    resized_image.thumbnail(output_size)
+    resized_image.save(image_path)
+
+    return image_file_name
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = ProfileUpdateForm()
+    if form.validate_on_submit():
+        if current_user.username==form.username.data and current_user.email==form.email.data and form.image.data==None:
+            flash('No change found!', 'info')
+            return redirect(url_for('account'))
+        if form.image.data:
+            image_file_name = save_image(form.image.data)
+            if current_user.image_file != 'default.jpg':
+                image_path_delete = os.path.join(app.root_path, 'static/profile_pics', current_user.image_file)
+                os.remove(image_path_delete)
+            current_user.image_file = image_file_name
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your profile has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename=f'profile_pics/{ current_user.image_file}')
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
